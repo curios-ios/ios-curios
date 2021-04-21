ff62efc166c3be70b4a1dc525fcd590f8932587a
Vivian Lin <linvivian61@gmail.com>
2021-04-21 12:23:12 -0400

    login and register

---
+++ Pods/Target Support Files/Pods-InstaRecipe-InstaRecipeUITests/Pods-InstaRecipe-InstaRecipeUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_InstaRecipe_InstaRecipeUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_InstaRecipe_InstaRecipeUITestsVersionString[];
+
