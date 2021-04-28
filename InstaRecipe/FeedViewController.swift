6335590fc5e14f68535fc5815a957830b461d55e
Md Sadiq Sada <samin.sada@gmail.com>
2021-04-28 16:12:38 -0400

    view recipe queries done

--- InstaRecipe/FeedViewController.swift
+++ InstaRecipe/FeedViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -16,16 +17,49 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Recipes")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground{(recipes, error) in
+            if recipes != nil {
+                self.recipes = recipes!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return recipes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell") as! RecipeCell
+        
+        let recipe = recipes[indexPath.row]
+        let user = recipe["author"] as! PFUser
+        
+        cell.nameLabel.text = user.username
+        cell.descriptionLabel.text = recipe["description"] as? String
+        
+        // recipe image
+        let imageFile = recipe["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.recipeImageView.af.setImage(withURL: url)
+        
+        
+        return cell
     }
     
 
